0d96d98d06ca24b4dc7dca08f499a3e38741c932
Steven <stevenhiek@gmail.com>
2021-10-07 10:59:00 -0700

    Added flask folder

---
+++ Steven/Flask/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, redirect
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float 
+import pandas as pd
+
+
+# import pymongo
+
+
+rds_connection_string = "postgres:postgres@localhost:5432/restaurants_db"
+engine = create_engine(f'postgresql://{rds_connection_string}')
+data = pd.read_sql_query('select * from restaurants', con=engine)
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def home():
+
+    data_html = data.to_html(index=False)
+    
+    # Return template and data
+    return render_template("/index.html", topten = data_html)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ Steven/Flask/static/js/plots.js
@@ -0,0 +1,53 @@
+// Use d3 to read the JSON file.
+// The data from the JSON file is arbitrarily named importedData as the argument.
+d3.json("/static/data/az_yelp_restaurants.json", function (importedData)  {
+    console.log(importedData);
+    var data = importedData;
+    y= [1, 2, 3, 4, 5]
+
+
+// names = data.map(function (row){
+//     return row.city
+//   });
+  
+
+    // Trace1 for the Greek data.
+    var trace1 = {
+      x: data.map(row => row.city),
+      y: data.map(row => row.stars),
+      // text: data.map(row => row.stars),
+      
+      type: "bar",
+    //   orientation: "h"
+    };
+  
+    // Data
+    var chartData = [trace1];
+  
+    // Apply the group bar mode to the layout.
+    var layout = {
+      title: "Star ratings of restaurants in cities of AZ",
+      xaxis: {title: 'Cities in AZ',
+      },
+      yaxis: {
+        
+        title: 'Star Ratings',
+        
+    // autorange: true,
+    //  },
+    //   autosize: false,
+    //   width: 1000,
+    //   height: 800,
+    //   margin: {
+    //     l: 50,
+    //     r: 50,
+    //     b: 200,
+    //     t: 200,
+    //     pad: 4
+    //   },
+      
+    }};
+  
+    // Render the plot to the div tag with the id of "plot".
+    Plotly.newPlot("plotk", chartData, layout);
+  });
